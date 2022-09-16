939a36333cee77750c6192b2d4e5b2338a700574
IgorLuigi <igor.luigi@hotmail.com>
2022-09-16 10:00:43 -0300

    Repository para as Classes de Domínio

---
+++ src/main/java/com/igorluigi/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.igorluigi.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.igorluigi.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    
+}
